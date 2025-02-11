759b88104dc7ad4512e7c2184db8e1382dcda992
Cristian Rojas <crojas@clippers.com>
2025-02-11 11:18:49 -0500

    Fix gitignore, remove app after test

--- src/main/java/com/java/base/BaseTest.java
+++ src/main/java/com/java/base/BaseTest.java
@@ -2,11 +2,11 @@ package com.java.base;
 
 import static com.java.utils.Capabilities.getAndroidCapabilities;
 
-import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import java.net.MalformedURLException;
 import java.net.URL;
-import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 public class BaseTest {
 
@@ -25,8 +25,13 @@ public class BaseTest {
     }
   }
 
-  @BeforeSuite
+  @BeforeMethod
   public void setUp() {
     driver = createAndroidDriver();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    getDriver().removeApp("com.wdiodemoapp");
   }
 }
