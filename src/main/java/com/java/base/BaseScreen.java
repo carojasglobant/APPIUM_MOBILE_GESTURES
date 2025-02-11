48a1c61bdf4868b52f08020cf994de01be0a81a1
Cristian Rojas <crojas@clippers.com>
2025-02-11 14:08:27 -0500

    Fix pom dependencies to see the allure steps

--- src/main/java/com/java/base/BaseScreen.java
+++ src/main/java/com/java/base/BaseScreen.java
@@ -3,6 +3,7 @@ package com.java.base;
 import static com.java.utils.WaitActions.isTheElementVisible;
 
 import io.appium.java_client.android.AndroidDriver;
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebElement;
 
 public class BaseScreen {
@@ -17,6 +18,7 @@ public class BaseScreen {
     return driver;
   }
 
+  @Step("Click on {1}")
   public static void click(WebElement element, String description) {
     isTheElementVisible(element, 5);
     element.click();
