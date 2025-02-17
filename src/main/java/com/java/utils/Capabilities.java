a9b99a06836c90743eba046bd6213f8d30b9744f
Cristian Rojas <crojas@clippers.com>
2025-02-17 10:58:59 -0500

    add env file and add DotEnv pom dependency

--- src/main/java/com/java/utils/Capabilities.java
+++ src/main/java/com/java/utils/Capabilities.java
@@ -1,15 +1,21 @@
 package com.java.utils;
 
 import io.appium.java_client.android.options.UiAutomator2Options;
+import io.github.cdimascio.dotenv.Dotenv;
 
 public class Capabilities {
 
   public static UiAutomator2Options getAndroidCapabilities() {
+    Dotenv dotenv = Dotenv.configure()
+      .directory("src/test/resources")
+      .filename("env")
+      .load();
+
     return new UiAutomator2Options()
-      .setApp("/Users/ca.rojas/Downloads/android.wdio.native.app.v1.0.8.apk")
-      .setDeviceName("wasa 14")
-      .setPlatformName("Android")
-      .setPlatformVersion("14")
+      .setApp(dotenv.get("MOBILE.APP.URL"))
+      .setDeviceName(dotenv.get("MOBILE.DEVICE.NAME"))
+      .setPlatformName(dotenv.get("MOBILE.PLATFORM.VERSION"))
+      .setPlatformVersion(dotenv.get("MOBILE.FULL.RESET"))
       .setFullReset(true);
   }
 }
