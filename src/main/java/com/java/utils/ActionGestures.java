8335cfb19548ccde1fa6d798a75ef0b41820b422
Cristian Rojas <crojas@clippers.com>
2025-02-20 11:06:32 -0500

    Clean repo

--- src/main/java/com/java/utils/ActionGestures.java
+++ src/main/java/com/java/utils/ActionGestures.java
@@ -16,25 +16,25 @@ public class ActionGestures {
    * Swiping the screen vertically by percentage
    */
   @Step("Swipe down")
-  public static void swipeDown() {
+  public static void swipe(int percentageStartX, int percentageStartY, int percentageEndX,
+                           int percentageEndY) {
     int width = getDriver().manage().window().getSize().getWidth();
     int height = getDriver().manage().window().getSize().getHeight();
-    int centerX = width / 2;
-    int startY = (int) (height * 0.5);
-    int endY = (int) (height * 0.1);
+    int startPositionX = width * percentageStartX;
+    int endPositionX = width * percentageEndX;
+    int startPositionY = height * percentageStartY;
+    int endPositionY = height * percentageEndY;
 
-    PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+    PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
 
-    Sequence sequence = new Sequence(finger1, 0)
+    Sequence sequence = new Sequence(finger, 0)
       .addAction(
-        finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), centerX, startY))
-      .addAction(
-        finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
-      .addAction(
-        finger1.createPointerMove(Duration.ofMillis(600), PointerInput.Origin.viewport(), centerX,
-          endY))
-      .addAction(
-        finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+        finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), startPositionX,
+          startPositionY))
+      .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+      .addAction(finger.createPointerMove(Duration.ofMillis(600), PointerInput.Origin.viewport(),
+        endPositionX, endPositionY))
+      .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
     getDriver().perform(List.of(sequence));
   }
 
@@ -97,13 +97,53 @@ public class ActionGestures {
       .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
       .addAction(new Pause(finger, Duration.ofMillis(100)))
       .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
-
     getDriver().perform(List.of(sequence));
+  }
 
+  @Step("Double Tap on {0}")
+  public static void tap(WebElement element) {
+    int X = (element.getSize().width / 2) + element.getRect().getX();
+    int Y = (element.getSize().height / 2) + element.getRect().getY();
+
+    PointerInput finger = new PointerInput(PointerInput.Kind.TOUCH, "finger");
+    Sequence sequence = new Sequence(finger, 0)
+      .addAction(finger.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), X, Y))
+      .addAction(finger.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+      .addAction(new Pause(finger, Duration.ofMillis(100)))
+      .addAction(finger.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+    getDriver().perform(List.of(sequence));
   }
 
   @Step("Navigate back")
   public static void navigateBack() {
     getDriver().navigate().back();
+  }
+
+  @Step("Drag {0} and drop into {1}")
+  public static void zoomIn(WebElement element, int distance) {
+    int fingerOneStartPositionX = element.getRect().getX();
+    int fingerOneStartPositionY = element.getRect().getY();
+    int fingerOneEndPositionX = (element.getRect().getX() - distance);
+    int fingerOneEndPositionY = (element.getRect().getY() - distance);
+    int fingerTwoStartPositionX = element.getSize().width + element.getRect().getX();
+    int fingerTwoStartPositionY = element.getSize().height + element.getRect().getY();
+    int fingerTwoEndPositionX = element.getSize().width + element.getRect().getX() + distance;
+    int fingerTwoEndPositionY = element.getSize().height + element.getRect().getY() + distance;
+
+    PointerInput finger1 = new PointerInput(PointerInput.Kind.TOUCH, "finger1");
+    PointerInput finger2 = new PointerInput(PointerInput.Kind.TOUCH, "finger2");
+
+    Sequence sequence1 = new Sequence(finger1, 0)
+      .addAction(finger1.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), fingerOneStartPositionX, fingerOneStartPositionY))
+      .addAction(finger1.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+      .addAction(finger1.createPointerMove(Duration.ofMillis(500), PointerInput.Origin.viewport(), fingerOneEndPositionX, fingerOneEndPositionY))
+      .addAction(finger1.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+
+    Sequence sequence2 = new Sequence(finger2, 0)
+      .addAction(finger2.createPointerMove(Duration.ZERO, PointerInput.Origin.viewport(), fingerTwoStartPositionX, fingerTwoStartPositionY))
+      .addAction(finger2.createPointerDown(PointerInput.MouseButton.LEFT.asArg()))
+      .addAction(finger2.createPointerMove(Duration.ofMillis(500), PointerInput.Origin.viewport(), fingerTwoEndPositionX, fingerTwoEndPositionY))
+      .addAction(finger2.createPointerUp(PointerInput.MouseButton.LEFT.asArg()));
+    getDriver().perform(List.of(sequence1, sequence2));
   }
 }
--- src/test/java/com/java/ActionGesturesTest.java
+++ src/test/java/com/java/ActionGesturesTest.java
@@ -1,22 +1,7 @@
 package com.java;
 
 import com.java.base.BaseTest;
-import com.java.screens.HomeScreen;
-import org.testng.annotations.Test;
 
 public class ActionGesturesTest extends BaseTest {
 
-  @Test(testName = "Open the app and validate the home screen")
-  public void test_01() {
-    HomeScreen homeScreen = new HomeScreen(driver);
-    homeScreen.isTheHomeScreenDisplayed();
-  }
-
-  @Test(testName = "Double Tap with W3C actions")
-  public void test_02() {
-    HomeScreen homeScreen = new HomeScreen(driver);
-    homeScreen.isTheHomeScreenDisplayed();
-    homeScreen.tapTwoTimesWithW3Cations();
-    homeScreen.isTheHomeScreenDisplayed();
-  }
 }
